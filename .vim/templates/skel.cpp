7d9abc0bf749bfc59f771300b3ea7c7d41cddb8d
Adi Islam <62735486+AdiIslam1@users.noreply.github.com>
2021-05-13 12:12:03 +0600

    Create skel.cpp
    Cpp template

---
+++ .vim/templates/skel.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define fastyshit ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(v) v.begin(),v.end()
+#define mx(v)  *max_element(v.begin(),v.end())
+#define mn(v)  *min_element(v.begin(),v.end())
+#define sum(v) accumulate(v.begin(),v.end(),0)
+#define uni(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define     Pair  pair<ll,ll>
+#define F  first
+#define pb push_back
+#define S  second
+#define endl "\n"
+typedef long long ll;
+using namespace std;
+
+int main(){
+    fastyshit
+     
+    return 0;
+}
